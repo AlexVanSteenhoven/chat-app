817ffdbd690532f0fbc7aa9fd958a189efb6655f
AlexVanSteenhoven <alex.steenhoven@gmail.com>
2019-10-21 22:07:26 +0200

    Added socket.io

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// Import Modules
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+// Create the server
+let app = express();
+let server = http.createServer(app);
+// Integrate socket.io with http and express
+let io = socketIO(server);
+
+// Set static path
+const publicPath = path.join(__dirname, '/../public');
+app.use(express.static(publicPath));
+
+// Define port and start server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`[Server] running on http://localhost:${port}`);
+});
